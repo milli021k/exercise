a9b2e4eb6796a08992b73edbf6ac5d85c17972e1
unknown <milli2021k@gmail.com>
2020-12-19 11:48:54 -0800

    first commit

---
+++ public/src/accounts.js
@@ -0,0 +1,42 @@
+const findAccountById = (accounts, id)=>accounts.find(account=> account.id === id);
+
+
+const sortAccountsByLastName = (accounts)=>accounts.sort((accountA,accountB)=>accountA.name.last>accountB.name.last?1:-1);
+
+
+function numberOfBorrows(account, books) 
+  {
+    const {id} = account;
+    let accumulator = 0;
+    books.forEach(book =>  
+      {
+      if(book.borrows.find(transaction => transaction.id === id))
+        {
+          accumulator++;
+        }
+    });
+   return  accumulator;  
+}
+
+
+
+function booksInPossession({id}, books, authors) {
+
+  let hasBook = book=>book.borrows.some(borrow => (borrow.id === id && !borrow.returned ));
+  let borrowedBooks = books.filter(book => hasBook(book));
+  
+  return borrowedBooks.reduce((acc, book) => { 
+    book.author = authors.find(author => author.id === book.authorId); 
+     acc.push(book);
+  return acc;  }, []);
+}
+
+
+
+
+module.exports = {
+findAccountById,
+sortAccountsByLastName,
+numberOfBorrows,
+booksInPossession,
+};
---
+++ public/src/books.js
@@ -0,0 +1,36 @@
+const findAuthorById = (authors, id)=>authors.find(author => author.id===id);
+  
+const findBookById=(books, id) =>books.find(book => book.id===id);
+  
+function partitionBooksByBorrowedStatus(books) {
+  let availableBooks = books.filter(book => book.borrows.every(borrow => (borrow.returned ===true )));
+  let borrowedBooks = books.filter(book => book.borrows.some(borrow => (borrow.returned === false)));
+  let result = [borrowedBooks,availableBooks]
+  return (result.length>0)?result:[]; 
+  
+}
+
+function getBorrowersForBook(book, accounts) {
+  let borrowers=[]; 
+  let borrowersInfo = book.borrows;
+  accounts.forEach(account=>
+     { 
+     const  {id } = account;
+      borrowersInfo.forEach(transaction =>{
+      if(id===transaction.id)
+        {
+          let result={...account}
+          result["returned"]=transaction.returned ;
+          borrowers.push(result);
+         }
+      })
+  });                     
+  return borrowers.slice(0,10);
+  }
+  
+module.exports = {
+  findAuthorById,
+  findBookById,
+  partitionBooksByBorrowedStatus,
+  getBorrowersForBook,
+};
